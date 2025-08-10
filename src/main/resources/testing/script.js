75a136eef6bab7f559d826c723d0e3134bf93526
AftabJ09 <aftab.50100@gmail.com>
2025-08-10 19:17:38 +0530

    updating script file

--- src/main/resources/testing/script.js
+++ src/main/resources/testing/script.js
@@ -1,4 +1,4 @@
-const quill = new Quill('#editor', {
+ const quill = new Quill('#editor', {
   theme: 'snow',
   placeholder: 'Write your blog content here...',
   modules: {
@@ -8,39 +8,8 @@ const quill = new Quill('#editor', {
       [{ list: 'ordered' }, { list: 'bullet' }],
       ['link', 'image'],
       ['clean']
-    ],
-    handlers:{
-    image:imageHandler
-    }
+    ]
   }
 });
 
-previewBtn.addEventListener('click', function () {
-  const contentHtml = quill.root.innerHTML;
-  let html = '';
-  html += `<div>${contentHtml}</div><button id="saveBtn">Save</button>`;
-  previewArea.innerHTML = html;
-  document.getElementById('saveBtn').addEventListener('click', saveBlog);
-});
 
-function imageHandler(){
-    const input = document.createElement('input');
-    input.setAttribute('type','file');
-    input.setAttribute('accept','image/*');
-    input.click();
-    input.onchange = async () => {
-        const file = input.files[0];
-        const formData = new FormData();
-        formData.append('image', file);
-    const res = await fetch('/upload-image', {
-      method: 'POST',
-      body: formData
-    });
-    const data = await res.json();
-    const range = quill.getSelection();
-    quill.insertEmbed(range.index, 'image', data.url);
-}
-function saveBlog() {
-  let delta = quill.getContents();
-  console.log(JSON.stringify(delta));
-}
